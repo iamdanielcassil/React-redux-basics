8697e9a480f167638772c967f153a07327b7963b
iamdanielcassil <40176656+iamdanielcassil@users.noreply.github.com>
2019-09-22 23:18:52 -0500

    fix refresh caused by included bonudles

--- src/bundles/index.js
+++ src/bundles/index.js
@@ -1,8 +1,15 @@
-import { composeBundles } from "redux-bundler";
+import { composeBundlesRaw, createUrlBundle } from "redux-bundler";
 import user from "./user";
 import states from "./states";
 import routes from "./routes";
 import races from "./races";
 import seasons from "./seasons";
 
-export default composeBundles(user, states, routes, races, seasons);
+export default composeBundlesRaw(
+  createUrlBundle(),
+  user,
+  states,
+  routes,
+  races,
+  seasons
+);
