c4582418364367c0e5dbe97fb9c9ed4664698fc6
iamdanielcassil <40176656+iamdanielcassil@users.noreply.github.com>
2019-10-06 15:29:32 -0500

    save

--- src/pages/EditBoat.js
+++ src/pages/EditBoat.js
@@ -9,27 +9,81 @@ export default connect(
 
     return (
       <div className="form">
-        <label htmlFor="name">
-          Name
-          <input
-            id="name"
-            className="field"
-            type="text"
-            onChange={e => setBoat({ ...boat, name: e.target.value })}
-            value={boat.name || ""}
-          />
-        </label>
-        <label htmlFor="captain">
-          Captain
-          <input
-            id="captain"
-            className="field"
-            type="text"
-            onChange={e => setBoat({ ...boat, captain: e.target.value })}
-            value={boat.captain || ""}
-          />
-        </label>
-        <input type="button" onClick={() => doSaveBoat(boat)} value="save" />
+        <div className="row">
+          <div className="column">
+            <label htmlFor="name">
+              Name
+              <input
+                id="name"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, name: e.target.value })}
+                value={boat.name || ""}
+              />
+            </label>
+            <label htmlFor="captain">
+              Captain
+              <input
+                id="captain"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, captain: e.target.value })}
+                value={boat.captain || ""}
+              />
+            </label>
+            <label htmlFor="maker">
+              Boat Maker
+              <input
+                id="maker"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, maker: e.target.value })}
+                value={boat.captain || ""}
+              />
+            </label>
+          </div>
+          <div className="column">
+            <label htmlFor="model">
+              Model
+              <input
+                id="model"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, model: e.target.value })}
+                value={boat.captain || ""}
+              />
+            </label>
+            <label htmlFor="length">
+              Length
+              <input
+                id="length"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, length: e.target.value })}
+                value={boat.captain || ""}
+              />
+            </label>
+            <label htmlFor="phrf">
+              PHRF
+              <input
+                id="phrf"
+                className="field"
+                type="text"
+                onChange={e => setBoat({ ...boat, phrf: e.target.value })}
+                value={boat.captain || ""}
+              />
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="double-column">
+            <input
+              type="button"
+              onClick={() => doSaveBoat(boat)}
+              value="save"
+            />
+          </div>
+        </div>
       </div>
     );
   }
